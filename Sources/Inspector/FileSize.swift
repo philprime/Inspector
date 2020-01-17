93ae092992937c285807aef03539259bef7401e3
Philip Niedertscheider <phil@techprimate.com>
2020-01-17 10:17:25 +0100

    Added missing public declarations

--- Sources/Inspector/FileSize.swift
+++ Sources/Inspector/FileSize.swift
@@ -21,84 +21,84 @@ public class FileSize {
     // MARK: - Decimal prefix
 
     /// Kibibyte
-    var KiB: Int64 {
+    public var KiB: Int64 {
         bytes / 1024
     }
 
     /// Mebibyte
-    var MiB: Int64 {
+    public var MiB: Int64 {
         KiB / 1024
     }
 
     /// Gibibyte
-    var GiB: Int64 {
+    public var GiB: Int64 {
         MiB / 1024
     }
 
     /// Tebibyte
-    var TiB: Int64 {
+    public var TiB: Int64 {
         GiB / 1024
     }
 
     /// Pebibyte
-    var PiB: Int64 {
+    public var PiB: Int64 {
         TiB / 1024
     }
 
     /// Exbibyte
-    var EiB: Int64 {
+    public var EiB: Int64 {
         PiB / 1024
     }
 
     /// Zebibyte
-    var ZiB: Int64 {
+    public var ZiB: Int64 {
         MiB / 1024
     }
 
     /// Yobibyte
-    var YiB: Int64 {
+    public var YiB: Int64 {
         ZiB / 1024
     }
 
     // MARK: - Binary prefix
 
     /// Kilobyte
-    var kB: Int64 {
+    public var kB: Int64 {
         bytes / 1000
     }
 
     /// Megabyte
-    var MB: Int64 {
+    public var MB: Int64 {
         kB / 1000
     }
 
     /// Gigabyte
-    var GB: Int64 {
+    public var GB: Int64 {
         MB / 1000
     }
 
     /// Terabyte
-    var TB: Int64 {
+    public var TB: Int64 {
         GB / 1000
     }
 
     /// Petabyte
-    var PB: Int64 {
+    public var PB: Int64 {
         TB / 1000
     }
 
     /// Exabyte
-    var EB: Int64 {
+    public var EB: Int64 {
         PB / 1000
     }
 
     /// Zettabyte
-    var ZB: Int64 {
+    public var ZB: Int64 {
         EB / 1000
     }
 
     /// Yottabyte
-    var YB: Int64 {
+    public var YB: Int64 {
         ZB / 1000
     }
 }
